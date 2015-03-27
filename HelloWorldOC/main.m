d377daff3b4646b1dc0786b35eb51f068fa237e9
chx20 <394468254@qq.com>
2015-03-27 15:47:17 +0800

    Initial Commit

---
+++ HelloWorldOC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorldOC
+//
+//  Created by 程红秀 on 2015-3-27.
+//  Copyright (c) 2015年 chx20. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
