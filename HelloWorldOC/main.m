659be9f844fa142f6303f2087fa04e358e1ca34e
chx20 <394468254@qq.com>
2015-03-27 15:51:59 +0800

    删除文件。。

--- HelloWorldOC/main.m
+++ HelloWorldOC/main.m
@@ -1,10 +1,19 @@
-//
-//  main.m
-//  HelloWorldOC
-//
-//  Created by 程红秀 on 2015-3-27.
-//  Copyright (c) 2015年 chx20. All rights reserved.
-//
+/**
+ xcode4.3引入ARC，release这块就有些变化，当你使用ARC，就必须将NSAutoreleasePool的地方换成 @autoreleasepool
+ 关于NSAutoreleasePool的解释官方的最清楚
+    Important If you use Automatic Reference Counting (ARC), 
+    you cannot use autorelease pools directly. Instead, 
+    you use @autoreleasepool blocks instead. For example, in place of:
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init;
+        // Code benefitting from a local autorelease pool.
+        [pool release];
+    you would write:
+        @autoreleasepool {
+            // Code benefitting from a local autorelease pool.
+        }
+    @autoreleasepool blocks are more efficient than using an instance of NSAutoreleasePool directly; 
+    you can also use them even if you do not use ARC.
+ **/
 
 #import <Foundation/Foundation.h>
 
